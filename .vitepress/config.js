41a182a6bbac20762ddf3986e44d2f4f4724cb28
Bruno Felaco <4786734+bfelaco@users.noreply.github.com>
2025-01-20 16:48:25 -0500

    Initial commit

---
+++ .vitepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    title: 'Bruno Felaco Personal Website',
+    description: 'This is my personal website hosted on GitHub Pages',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'About', link: '/about' },
+        { text: 'Projects', link: '/projects' }
+      ],
+      sidebar: [
+        {
+          text: 'Guide',
+          items: [
+            { text: 'Introduction', link: '/' },
+            { text: 'About', link: '/about' },
+            { text: 'Projects', link: '/projects' }
+          ]
+        }
+      ]
+    }
+  }
+  
---
+++ docs/index.md
@@ -0,0 +1,3 @@
+# Hello World
+
+This is the personal homepage of Bruno Felaco. I have nothing to say here yet.
